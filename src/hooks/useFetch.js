f86cca134b0c6df22ecf132b23da17ee3ba1da0c
Wilfrido Bonett <wbonett10@gmail.com>
2022-11-16 10:43:15 -0500

    custom hook created

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,47 @@
+import { useRef, useState, useEffect } from "react";
+
+export const useFetch = (url, _options) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = useRef(_options).current;
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+
+        const json = await res.json();
+
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          setIsPending(false);
+          setError(`The fetch was aborted`);
+        } else {
+          setIsPending(false);
+          setError(`Couldn't fetch the data.`);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url, options]);
+
+  return { data, isPending, error };
+};
